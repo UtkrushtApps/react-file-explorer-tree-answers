02f592abbe8d04b0db0e8e8e3bb9c285ee97a46c
Naman Muley <naman.g.muley@gmail.com>
2025-09-27 01:38:47 +0530

    Add src/App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FileExplorer from './FileExplorer';
+
+function App() {
+  return (
+    <div style={{ margin: 28, fontFamily: 'system-ui, sans-serif', background: '#f6f8fa', minHeight: '100vh', height: '100%' }}>
+      <FileExplorer />
+    </div>
+  );
+}
+
+export default App;
